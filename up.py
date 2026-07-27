2ddef2e1e312d328105ad5a664d9b13e34e3b131
subrahmanyammajeti <160775089+majetidattasrisubrahmanyam@users.noreply.github.com>
2026-07-27 09:09:50 +0530

    Implement classes A and B with display functionality
    Added classes A and B with inheritance and display methods.

--- up.py
+++ up.py
@@ -658,3 +658,30 @@ class student:
         print(self.name,self.age)
 
 
+class A:
+    a=10
+    b=20
+    def __init__(self,c,d):
+        self.c = c
+        self.d = d
+
+    def display(self):
+        print(self.c,self.d)
+
+
+class B(A):
+    a=90
+    def __init__(self,c,d,e,f,g):
+        super().__init__(c,d)
+        self.e = e
+        self.f=f
+        self.g=g
+
+    def display(self):
+        super().display()
+        print(self.e,self.f,self.g)
+
+oa = B(10,2,3,4,5)
+print(oa.display())
+
+
